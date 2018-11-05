b290931e6b9ca03ad619ac1f07b08ed397ce1ad9
hugs <2229313737@qq.com>
2018-11-05 11:56:13 +0800

    wx add profile

---
+++ src/component/Wx/Profile.js
@@ -0,0 +1,46 @@
+// 个人信息
+
+import React, { Component } from 'react';
+
+import "../../css/Wx/WxMyHome.css";
+
+class Profile extends Component{
+	
+constructor(porps){
+	
+	super(porps);
+};
+	
+render(){
+
+return(
+
+<div id="profile">
+			
+	<div>
+		
+		<img src=""/>
+		
+		<div>
+			
+			<h3>书简_yu</h3>
+			
+			<p>关注 粉丝</p>
+		</div>
+		
+	</div>
+	
+	<p>
+		
+			
+	</p>
+
+</div>
+
+)
+}
+	
+	
+}
+
+export default Profile;
