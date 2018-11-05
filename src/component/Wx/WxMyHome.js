2017ebce0d14610b1475241a0c1df67f982c297e
hugs <2229313737@qq.com>
2018-11-05 11:15:00 +0800

    rename 2

---
+++ src/component/Wx/WxMyHome.js
@@ -0,0 +1,24 @@
+// 我的主页
+
+import React, { Component } from 'react';
+
+class wxMyHome extends Component{
+	
+	constructor(porps){
+		
+		super(porps);
+	};
+	
+	render(){
+		
+		return(
+			
+			<div id="wrap">
+			
+				月亮很亮,
+			</div>
+		)
+	}
+}
+
+export default wxMyHome;
