fc0961716dfbbc87f9f171bb85e923b1ab1e558c
yangxiaolong <1329765493@qq.com>
2018-11-08 20:56:31 +0800

    yxl

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import Luyou from "./component/YXL/luyou";
+
+import Registered from "./component/YXL/registered";
+import Login from "./component/YXL/login";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/*<header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>*/}
-					
-					<h3 style={{height: 55, background: "beige", margin: 0, padding: 0, "line-height": 55}}>我的主页</h3>
-				
+          <Luyou/>
+          
+              
+              
       </div>
     );
   }
