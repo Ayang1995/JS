bc1618cf50c6cd42966b7e812de679015f6185a8
hugs <2229313737@qq.com>
2018-11-08 19:25:30 +0800

    all2

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import React, { Component } from 'react';
-
+import logo from './logo.svg';
 import './App.css';
-
-import MyHome from './component/Wx/MyHome.js';
-
-import Note from './component/Wx/Note.js';
-
-import {BrowserRouter as Router, Route, Link, Redirect, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-		
-		<Router>
-		<div className="App">
+      <div className="App">
+        {/*<header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>*/}
 					
-			{/*<h3 style={{height: 55, background: "beige", margin: 0, padding: 0, "lineHeight": "55px"}}>我的主页</h3>*/}
-			
-			<Switch>
-			
-			<Route path='/user' component={MyHome}></Route>
-			
-			<Route path='/note' component={Note}></Route>
-			
-			<Redirect to='/note' />
-			
-			</Switch>
+					<h3 style={{height: 55, background: "beige", margin: 0, padding: 0, "line-height": 55}}>我的主页</h3>
 				
-		</div>
-		</Router>
+      </div>
     );
   }
 }
